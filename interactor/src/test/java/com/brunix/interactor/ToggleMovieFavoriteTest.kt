12b75adbd5b7aab86eb4f800177bdee7849e57ea
Dolo <dologij@gmail.com>
2020-02-12 21:26:59 +0100

    Add tests for FindMovieById and ToggleMovieFavorite interactors

---
+++ interactor/src/test/java/com/brunix/interactor/ToggleMovieFavoriteTest.kt
@@ -0,0 +1,77 @@
+package com.brunix.interactor
+
+import com.brunix.data.repository.MoviesRepository
+import com.brunix.domain.Movie
+import com.nhaarman.mockitokotlin2.verify
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class ToggleMovieFavoriteTest {
+
+    val mockedMovie = Movie(
+        0,
+        "Title",
+        "Overview",
+        "01/01/2025",
+        "",
+        "",
+        "EN",
+        "Title",
+        5.0,
+        5.1,
+        false
+    )
+
+    @Mock
+    lateinit var moviesRepository: MoviesRepository
+
+    lateinit var toggleMovieFavorite: ToggleMovieFavorite
+
+    @Before
+    fun setUp() {
+        toggleMovieFavorite = ToggleMovieFavorite(moviesRepository)
+    }
+
+    @Test
+    fun `invoke calls movies repo`() {
+        runBlocking {
+
+            val movie = mockedMovie.copy(4)
+
+            val result = toggleMovieFavorite.invoke(movie)
+
+            verify(moviesRepository).update(result)
+        }
+    }
+
+    @Test
+    fun `favorite movie gets unfavorited`() {
+        runBlocking {
+
+            val movie = mockedMovie.copy(favorite = true)
+
+            val result = toggleMovieFavorite.invoke(movie)
+
+            assertFalse(result.favorite)
+        }
+    }
+
+    @Test
+    fun `unfavorite movie gets favorited`() {
+        runBlocking {
+
+            val movie = mockedMovie.copy(favorite = false)
+
+            val result = toggleMovieFavorite.invoke(movie)
+
+            assertTrue(result.favorite)
+        }
+    }
+}
