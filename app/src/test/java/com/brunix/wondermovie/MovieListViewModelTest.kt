7aa6227adb8e70fad87f54ecf097dfc41702ca6a
Dolo <dologij@gmail.com>
2020-02-15 17:21:14 +0100

    Add tests for MovieList viewmodel

---
+++ app/src/test/java/com/brunix/wondermovie/MovieListViewModelTest.kt
@@ -0,0 +1,85 @@
+package com.brunix.wondermovie
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
+import com.brunix.domain.Movie
+import com.brunix.interactor.GetPopularMovies
+import com.brunix.wondermovie.ui.main.MovieListViewModel
+import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.whenever
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class MovieListViewModelTest {
+
+    @get:Rule
+    val rule = InstantTaskExecutorRule()
+
+    @Mock
+    lateinit var getPopularMovies: GetPopularMovies
+
+    @Mock
+    lateinit var observer: Observer<MovieListViewModel.UiModel>
+
+    private lateinit var viewmodel: MovieListViewModel
+
+    private val testDispatcher = TestCoroutineDispatcher()
+
+    private val mockedMovie = Movie(
+        0,
+        "Title",
+        "Overview",
+        "01/01/2025",
+        "",
+        "",
+        "EN",
+        "Title",
+        5.0,
+        5.1,
+        false
+    )
+
+    @Before
+    fun setUp() {
+        viewmodel = MovieListViewModel(getPopularMovies)
+        Dispatchers.setMain(testDispatcher)
+    }
+
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain()
+        testDispatcher.cleanupTestCoroutines()
+    }
+
+    @Test
+    fun `observing LiveData executes location permission request`() {
+        viewmodel.model.observeForever(observer)
+
+        verify(observer).onChanged(MovieListViewModel.UiModel.RequestLocationPermission)
+    }
+
+    @Test
+    fun `loading is shown after requesting location permission`() {
+        runBlocking {
+
+            val movies = listOf(mockedMovie.copy(1))
+            whenever(getPopularMovies.invoke()).thenReturn(movies)
+
+            viewmodel.model.observeForever(observer)
+            viewmodel.onCoarsePermissionRequested()
+
+            verify(observer).onChanged(MovieListViewModel.UiModel.Loading)
+        }
+    }
+}
