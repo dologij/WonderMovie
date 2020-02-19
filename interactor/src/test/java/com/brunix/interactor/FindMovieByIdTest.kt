69e19bdfe0d32061a89a102d03fc85621c9bcccf
Dolo <dologij@gmail.com>
2020-02-19 19:53:51 +0100

    Add tests for FindMovieById and ToggleMovieFavorite interactors

---
+++ interactor/src/test/java/com/brunix/interactor/FindMovieByIdTest.kt
@@ -0,0 +1,53 @@
+package com.brunix.interactor
+
+import com.brunix.data.repository.MoviesRepository
+import com.brunix.domain.Movie
+import com.nhaarman.mockitokotlin2.whenever
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class FindMovieByIdTest {
+
+    val mockedMovie = Movie(
+        0,
+        "Title",
+        "Overview",
+        "01/01/2025",
+        "",
+        "",
+        "EN",
+        "Title",
+        5.0,
+        5.1,
+        false
+    )
+
+    @Mock
+    lateinit var moviesRepository: MoviesRepository
+
+    lateinit var findMovieById: FindMovieById
+
+    @Before
+    fun setUp() {
+        findMovieById = FindMovieById(moviesRepository)
+    }
+
+    @Test
+    fun `invoke calls movies repo`() {
+        runBlocking {
+
+            val movie = mockedMovie.copy(2)
+            whenever(moviesRepository.findById(2)).thenReturn(movie)
+
+            val result = findMovieById.invoke(2)
+
+            Assert.assertEquals(movie, result)
+        }
+    }
+}
