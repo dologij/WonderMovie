14c3d2fee8471641e3b362f46091b7eff37349aa
Dolo <dologij@gmail.com>
2020-02-19 19:48:57 +0100

    Add tests for RegionRepository

--- data/src/main/java/com/brunix/data/repository/RegionRepository.kt
+++ data/src/main/java/com/brunix/data/repository/RegionRepository.kt
@@ -8,7 +8,7 @@ class RegionRepository(
 ) {
 
     companion object {
-        private const val DEFAULT_REGION = "US"
+        const val DEFAULT_REGION = "US"
     }
 
     suspend fun findLastRegion(): String {
---
+++ data/src/test/java/com/brunix/data/repository/RegionRepositoryTest.kt
@@ -0,0 +1,52 @@
+package com.brunix.data.repository
+
+import com.brunix.data.source.LocationDataSource
+import com.nhaarman.mockitokotlin2.whenever
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class RegionRepositoryTest {
+
+    @Mock
+    lateinit var locationDataSource: LocationDataSource
+
+    @Mock
+    lateinit var permissionChecker: PermissionChecker
+
+    private lateinit var regionRepository: RegionRepository
+
+    @Before
+    fun setUp() {
+        regionRepository = RegionRepository(locationDataSource, permissionChecker)
+    }
+
+    @Test
+    fun `returns default when coarse perm is not granted`() {
+        runBlocking {
+            whenever(permissionChecker.check(PermissionChecker.Permission.COARSE_LOCATION)).thenReturn(false)
+
+            val region = regionRepository.findLastRegion()
+
+            Assert.assertEquals(RegionRepository.DEFAULT_REGION, region)
+        }
+    }
+
+    @Test
+    fun `returns region from location data source when perm is granted`() {
+        runBlocking {
+
+            whenever(permissionChecker.check(PermissionChecker.Permission.COARSE_LOCATION)).thenReturn(true)
+            whenever(locationDataSource.findLastRegion()).thenReturn("FR")
+
+            val region = regionRepository.findLastRegion()
+
+            Assert.assertEquals("FR", region)
+        }
+    }
+}
